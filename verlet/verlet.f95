d114050061a183e4cfca47988768eb525e6c4917
Lorenzo Dei Rossi <lorenzo.deirossi@studenti.unipd.it>
2025-10-31 16:36:48 +0100

    Include different timesteps

--- verlet/verlet.f95
+++ verlet/verlet.f95
@@ -7,46 +7,58 @@ PROGRAM verlet
         !parameter means that the value of the variable cannot change
         INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND (p=13, r=300) 
 
-        INTEGER :: k, n, m
+        INTEGER :: k, i, n, m, time
         REAL (KIND=wp) :: tau, f_x, f_y, f_z 
         REAL, DIMENSION(:), ALLOCATABLE :: x, y, z, v_x, v_y, v_z ! 1-index array
 
-        k = 0
-        n = 10          ! number of iteration
-        tau = 0.2       ! time-step    
-        m = 1
-        f_x = 0.0
-        f_y = 0.1
-        f_z = 0.0
+        k = 0 
+        time = 120       ! total time of the simulation
+        tau = 0.0        ! time-step       
+        m = 1            ! mass
+        f_x = 0.0        ! force in the x direction       
+        f_y = 0.1        ! force in the y direction
+        f_z = 0.0        ! force in the z direction
+ 
 
-        ALLOCATE( x(n), y(n), z(n), v_x(n), v_y(n), v_z(n) )
-
-        !Initialization of positions and velocities
-        x(1) = 0.0
-        y(1) = 0.0
-        z(1) = 0.0
-        v_x(1) = 0.0
-        v_y(1) = 0.0
-        v_z(1) = 0.0
-        
-        PRINT*, "STEP", 0
-        PRINT*, x(1), y(1), z(1)
-        PRINT*, v_x(1), v_y(1), v_z(1)
-
-        !Calculations of the positions and the velocities
-        DO k = 1, n
-                x(k+1) = x(k) + tau*v_x(k) + tau**2 * (f_x/2*m)
-                v_x(k+1) = v_x(k) + (tau/2*m)*(f_x + f_x)
+        DO i = 0, 100, 5
+                tau = 0.1+i   
+                n = INT(time / tau)
+                ALLOCATE( x(n), y(n), z(n), v_x(n), v_y(n), v_z(n) )
+                !PRINT*, "TIMESTEP" , tau
+                !PRINT*, "NUMBER OF STEPS", n
+               
                 
-                y(k+1) = y(k) + tau*v_y(k) + tau**2 * (f_y/2*m)
-                v_y(k+1) = v_y(k) + (tau/2*m)*(f_y + f_y)
+                !Initialization of positions and velocities
+                x(1) = 0.0
+                y(1) = 0.0
+                z(1) = 0.0
+                v_x(1) = 0.0
+                v_y(1) = 0.0
+                v_z(1) = 0.0
+                
+                
+               ! PRINT*, "STEP", 0
+               ! PRINT*, x(1), y(1), z(1)
+               ! PRINT*, v_x(1), v_y(1), v_z(1)
 
-                z(k+1) = z(k) + tau*v_z(k) + tau**2 * (f_z/2*m)
-                v_z(k+1) = v_z(k) + (tau/2*m)*(f_z + f_z)
+                !Calculations of the positions and the velocities
+                DO k = 1, n
+                        x(k+1) = x(k) + tau*v_x(k) + tau**2 * (f_x/2*m)
+                        v_x(k+1) = v_x(k) + (tau/2*m)*(f_x + f_x)
+                        
+                        y(k+1) = y(k) + tau*v_y(k) + tau**2 * (f_y/2*m)
+                        v_y(k+1) = v_y(k) + (tau/2*m)*(f_y + f_y)
 
-                PRINT*, "STEP", k 
-                PRINT*, x(k+1), y(k+1), z(k+1)
-                PRINT*, v_x(k+1), v_y(k+1),v_z(k+1)
+                        z(k+1) = z(k) + tau*v_z(k) + tau**2 * (f_z/2*m)
+                        v_z(k+1) = v_z(k) + (tau/2*m)*(f_z + f_z)
+
+                       ! PRINT*, "STEP", k 
+                       ! PRINT*, x(k+1), y(k+1), z(k+1)
+                       ! PRINT*, v_x(k+1), v_y(k+1),v_z(k+1)
+                ENDDO
+                
+                PRINT*,  y(n)
+                DEALLOCATE (x, y, z, v_x, v_y, v_z)
         ENDDO
 
 END PROGRAM verlet       
